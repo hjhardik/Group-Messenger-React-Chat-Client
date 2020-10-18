9caeb77d1a1aa49eee2ad3b29b58a326c9b7e340
Hardik Joshi <hardikj7321@gmail.com>
2020-10-18 23:17:00 +0530

    react-app-rewired usage

--- client/config-overrides.js
+++ client/config-overrides.js
@@ -1,3 +1,6 @@
+//take a look at this link https://dev.to/aryclenio/part-3-setting-up-a-template-with-react-app-rewired-3e9j
+//It explains the use and importance of react-app-rewired 
+//config-overrides.js exports the settings in which will be overwritten.
 const { compose } = require('react-app-rewired');
 const rewireEslint = require('react-app-rewire-eslint');
 const rewirePolyfills = require('react-app-rewire-polyfills');
