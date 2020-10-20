6cf15b0c79c971ca61adf40fb7f23e21de31ac36
Hardik Joshi <hardikj7321@gmail.com>
2020-10-20 08:34:14 +0530

    reducers comments complete

--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -15,6 +15,7 @@ export default combineReducers({
 export const getUserId = user => user._id;
 export const getActiveUser = state => state.auth.user;
 
+//checks if the user is creator of chat
 export const isCreator = (state, chat) => {
   try {
     return getUserId(chat.creator) === getUserId(getActiveUser(state));
@@ -23,6 +24,7 @@ export const isCreator = (state, chat) => {
   }
 };
 
+//checks if the user is member of chat
 export const isMember = (state, chat) => {
   try {
     return chat.members.some(member => getUserId(member) === getUserId(getActiveUser(state)));
@@ -30,5 +32,5 @@ export const isMember = (state, chat) => {
     return false;
   }
 };
-
+//returns true if user is either member or creator
 export const isChatMember = (state, chat) => isCreator(state, chat) || isMember(state, chat);
